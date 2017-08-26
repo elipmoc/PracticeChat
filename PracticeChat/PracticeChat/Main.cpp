cecce0dc3a4d8f3d672b0d8e2b562369cdb08a0f
a-to-ha <ath3739@gmail.com>
2017-08-27 00:37:19 +0900

    円を増やした。

--- PracticeChat/PracticeChat/Main.cpp
+++ PracticeChat/PracticeChat/Main.cpp
@@ -1,14 +1,13 @@
-﻿
-
+﻿// Goodbye include file!!!!
 void Main()
 {
 	const siv::Font font(30);
 
 	while (siv::System::Update())
 	{
-		// 20170827_ATH.
 		font(L"ようこそ、真のGithub の世界へ！").draw();
 
+		siv::Circle(siv::Mouse::Pos(), 100).draw({ 0, 255, 0, 127 });
 		siv::Circle(siv::Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
 	}
 }
