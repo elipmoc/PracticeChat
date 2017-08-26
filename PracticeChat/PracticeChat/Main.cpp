51614f35cf1051f4b4d97cc0a07798393e3409f4
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-08-27 00:29:22 +0900

    神社☆

--- PracticeChat/PracticeChat/Main.cpp
+++ PracticeChat/PracticeChat/Main.cpp
@@ -2,12 +2,12 @@
 
 void Main()
 {
+
 	const siv::Font font(30);
 
 	while (siv::System::Update())
 	{
 		font(L"ようこそ、真のGithub の世界へ！").draw();
-
 		siv::Circle(siv::Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
 	}
 }
