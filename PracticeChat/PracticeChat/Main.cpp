72b9a3af0d6059474e655d50ce803036d1f5093a
Sutaa <sutaahbts@yahoo.co.jp>
2017-08-27 01:02:22 +0900

    円を重ねた

--- PracticeChat/PracticeChat/Main.cpp
+++ PracticeChat/PracticeChat/Main.cpp
@@ -9,5 +9,7 @@ void Main()
 	{
 		font(L"ようこそ、真のGithub の世界へ！").draw();
 		siv::Circle(siv::Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
+		siv::Circle(siv::Mouse::Pos(), 20).draw({ 0, 127, 0, 127 });
+		siv::Circle(siv::Mouse::Pos(), 10).draw({ 0, 0, 127, 127 });
 	}
 }
