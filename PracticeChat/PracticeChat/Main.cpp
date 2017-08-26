2acd4248835e27af4693e085afe8916ec723024c
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-08-26 23:29:06 +0900

    名前空間を定義した

--- PracticeChat/PracticeChat/Main.cpp
+++ PracticeChat/PracticeChat/Main.cpp
@@ -1,14 +1,13 @@
-﻿
-# include <Siv3D.hpp>
+﻿#include "UseSiv3D.hpp"
 
 void Main()
 {
-	const Font font(30);
+	const siv::Font font(30);
 
-	while (System::Update())
+	while (siv::System::Update())
 	{
 		font(L"ようこそ、Siv3D の世界へ！").draw();
 
-		Circle(Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
+		siv::Circle(siv::Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
 	}
 }
