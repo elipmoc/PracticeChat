2f959993ae281a6fada37832f29f7c76c0938857
a-to-ha <ath3739@gmail.com>
2017-08-27 00:49:53 +0900

    今度こそ

--- PracticeChat/PracticeChat/Main.cpp
+++ PracticeChat/PracticeChat/Main.cpp
@@ -9,5 +9,6 @@ void Main()
 		font(L"ようこそ、真のGithub の世界へ！").draw();
 
 		siv::Circle(siv::Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
+		siv::Circle(siv::Mouse::Pos(), 100).draw({ 0, 255, 0, 127 });
 	}
 }
