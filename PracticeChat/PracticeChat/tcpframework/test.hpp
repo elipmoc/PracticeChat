0785c42df0fbe5a1fca56a5d558aef4aecd4f0c4
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-08-27 23:56:42 +0900

    コメント増量

--- PracticeChat/PracticeChat/tcpframework/test.hpp
+++ PracticeChat/PracticeChat/tcpframework/test.hpp
@@ -5,26 +5,26 @@
 
 namespace tcpframework {
 	
-
-
+	//API初期化クラス
 	class TcpManager {
 		WSADATA m_wsa_data;
 		TcpManager() = default;
 		static TcpManager* instance;
 	public:
 		
+		//API初期化処理
 		static bool Init() {
-			
 			if (instance != nullptr)return false;
 			instance = new TcpManager();
-			//API初期化処理
+			
 			int error=WSAStartup(MAKEWORD(2, 0), &instance->m_wsa_data);
 			if (error != 0)return false;
 			return true;
 		}
 
+		//API終了処理
 		static void End() {
-			//API終了処理
+			
 			WSACleanup();
 			delete instance;
 		}
@@ -38,9 +38,9 @@ namespace tcpframework {
 	public:
 		SendSocket(const SOCKET& sock,const sockaddr_in& addr):
 			m_sock(sock),m_addr(addr)
-		{
+		{}
 
-		}
+		//送信
 		int Send(const std::string& str) {
 			return send(m_sock, str.c_str(), str.size(), 0);
 
@@ -62,14 +62,17 @@ namespace tcpframework {
 			m_addr.sin_addr.S_un.S_addr = INADDR_ANY;
 		}
 
+		//ソケットに情報を紐づけ
 		bool Bind() {
 			return bind(m_sock, reinterpret_cast<sockaddr*>(&m_addr), sizeof(m_addr))==0;
 		}
 
+		//ソケットが接続待機できるようにする
 		bool Listen() {
 			return listen(m_sock, m_max_connect) == 0;
 		}
 
+		//接続を待機する
 		std::unique_ptr<SendSocket> Accept() {
 			sockaddr_in client;
 			int len = sizeof(client);
