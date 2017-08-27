27a00c7242e57e9597f7983136221f96dbe85769
Sutaa <sutaahbts@yahoo.co.jp>
2017-08-27 20:37:04 +0900

    RoomSceneにGUIを追加。ボタンで遷移させる。

--- PracticeChat/PracticeChat/chat/header/RoomScene.h
+++ PracticeChat/PracticeChat/chat/header/RoomScene.h
@@ -4,17 +4,26 @@
 class RoomScene : public chat::MySceneBase
 {
 	const siv::Font font;
+
+	siv::GUI gui;
+
 	public:
 		RoomScene():font(10){}
 
 	void init() override
 	{
+		gui = siv::GUI(siv::GUIStyle::Default);
+
+		gui.add(L"bt1", siv::GUIButton::Create(L"OK"));
+		gui.add(L"bt2", siv::GUIButton::Create(L"Cansel"));
+		
+		gui.setCenter(siv::Window::Center());
 
 	}
 
 	void update() override
 	{
-		if (siv::Input::MouseL.clicked)
+		if (gui.button(L"bt1").pushed)
 			changeScene(L"Login");
 
 	}
