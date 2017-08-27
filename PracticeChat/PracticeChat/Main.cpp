7a7bc1ecf678c833f0e7ed3164c176c01cd0d4ed
a-to-ha <ath3739@gmail.com>
2017-08-27 22:52:15 +0900

    フォントテスト

--- PracticeChat/PracticeChat/Main.cpp
+++ PracticeChat/PracticeChat/Main.cpp
@@ -1,15 +1,6 @@
-﻿void Main()
+﻿#include <FontExperiment.h>
+void Main()
 {
-
-	const siv::Font font(30);
-
-	while (siv::System::Update())
-	{
-		font(L"ようこそ、真のGithub の世界へ！").draw();
-		siv::Circle(siv::Mouse::Pos(), 100).draw({ 127, 127, 127, 127 });
-		siv::Circle(siv::Mouse::Pos(), 70).draw({ 255, 255, 0, 127 });
-		siv::Circle(siv::Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
-		siv::Circle(siv::Mouse::Pos(), 20).draw({ 0, 127, 0, 127 });
-		siv::Circle(siv::Mouse::Pos(), 10).draw({ 0, 0, 127, 127 });
-	}
+	FontExperiment fe;
+	fe.MainLoop();
 }
