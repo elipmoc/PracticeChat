146c69482396d58adfe9e5d1aeeaba2aff584e41
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-08-29 14:33:29 +0900

    TcpManagerを隔離した

--- PracticeChat/PracticeChat/tcpframework/test.hpp
+++ PracticeChat/PracticeChat/tcpframework/test.hpp
@@ -1,35 +1,9 @@
 #pragma once
+#include "TcpManager.hpp"
 #include <WinSock2.h>
-#include <memory>
 #include <string>
 
 namespace tcpframework {
-	
-
-
-	class TcpManager {
-		WSADATA m_wsa_data;
-		TcpManager() = default;
-		static TcpManager* instance;
-	public:
-		
-		static bool Init() {
-			
-			if (instance != nullptr)return false;
-			instance = new TcpManager();
-			//API初期化処理
-			int error=WSAStartup(MAKEWORD(2, 0), &instance->m_wsa_data);
-			if (error != 0)return false;
-			return true;
-		}
-
-		static void End() {
-			//API終了処理
-			WSACleanup();
-			delete instance;
-		}
-	};
-
 
 	//送信用ソケットクラス
 	class SendSocket {
