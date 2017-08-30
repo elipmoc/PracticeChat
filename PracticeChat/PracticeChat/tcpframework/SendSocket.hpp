914ae4b89dd2356910aa8af046a468c61d507c43
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-08-30 18:02:20 +0900

    UINT_PTRがｘ64ビルドでエラーする問題を修正

--- PracticeChat/PracticeChat/tcpframework/SendSocket.hpp
+++ PracticeChat/PracticeChat/tcpframework/SendSocket.hpp
@@ -2,12 +2,15 @@
 #include <memory>
 
 //くそのwinsockで定義されている型を前方宣言
+#ifdef	_WIN64
+	typedef unsigned __int64 UINT_PTR;
+#elif defined _WIN32
 	typedef unsigned int UINT_PTR;
+#endif 
+
 	typedef UINT_PTR SOCKET;
 	struct sockaddr_in;
 namespace tcpframework {
-
-
 
 	//送信用ソケットクラス
 	class SendSocket {
