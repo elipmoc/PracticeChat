d06134caacd86abbcbe045227b6d6d44132a227e
a-to-ha <ath3739@gmail.com>
2017-09-05 20:51:12 +0900

    Textクラスの追加

---
+++ PracticeChat/PracticeChat/Experiment/ATH/Text.cpp
@@ -0,0 +1,38 @@
+#include "Text.h"
+#include "M_Font.h"
+
+namespace experiment {
+	namespace ATH {
+		Text::Text(const siv::String& _text, int _fontSize, int _fontPattern, int _drawX, int _drawY, const siv::Color& _color)
+		{
+			text = _text;
+			fontSize = _fontSize;
+			fontPattern = _fontPattern;
+			drawX = _drawX;
+			drawY = _drawY;
+			font = experiment::ATH::M_Font::MakeFont(fontPattern, fontSize);
+		}
+
+		void Text::Update()
+		{
+		}
+		
+		void Text::Draw()
+		{
+			font(text).draw(drawX, drawY);
+		}
+
+		void Text::SetFontsize(int fontSize) noexcept {
+			font = experiment::ATH::M_Font::MakeFont(fontPattern, fontSize);
+		}
+
+		void Text::SetColor(const siv::Color& _color) noexcept
+		{
+			color = _color;
+		}
+
+		Text::~Text()
+		{
+		}
+	}
+}
--- PracticeChat/PracticeChat/Experiment/Test_A_TO_HA.hpp
+++ PracticeChat/PracticeChat/Experiment/Test_A_TO_HA.hpp
@@ -1,26 +1,28 @@
 #pragma once
-#include "ATH\FontManager.h"
+#include "ATH\M_Font.h"
+#include "ATH\Text.h"
+#include <memory>
 
 namespace experiment {
 	class Test_A_TO_HA {
-		ATH::FontManager fm;
+		std::unique_ptr<ATH::Text> text;
+		ATH::M_Font fm;
 		int fontsize = 16;
 		int frame = 0;
 	public:
-		void Init(){	
-			siv::Println(L"OK");
+		void Init() {
+			text = std::make_unique<ATH::Text>(L"名前スペース！",30, 1);
+			text->SetColor({ 255,127,0 });
 		}
 		void Update(){
 			frame++; frame %= 10;
 			if (frame == 0) {
 				fontsize++;
 				if (fontsize > 60) fontsize = 16;
-				for (int i = 0; i < 4; i++) fm.setFontsize(i, fontsize);
+				for (int i = 0; i < 4; i++) text->SetFontsize(fontsize);
 			}
-			fm.f[0](L"ネーム空間はタブー").draw(10,10);
-			fm.f[1](L"ネーム空間はタブー").draw(10,60);
-			fm.f[2](L"ネーム空間はタブー").draw(10,110);
-			fm.f[3](L"ネーム空間はタブー").draw(10,160);
+
+			text->Draw();
 		}
 		void End(){}
 	};
