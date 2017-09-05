444d748933a207e683b8d6c20eaf5357eb920ba9
パン粉 ライ麦 <relipmoc101@gmail.com>
2017-09-05 20:54:20 +0900

    進捗ダメです

--- PracticeChat/PracticeChat/Experiment/TestElipmoc.hpp
+++ PracticeChat/PracticeChat/Experiment/TestElipmoc.hpp
@@ -1,31 +1,38 @@
 #pragma once
 #include "../tcpframework/tcpframework.hpp"
+#include <fstream>
 namespace experiment {
 
 	class TestElipmoc {
-		std::unique_ptr<tcpframework::ServerSocket> server;
-		std::unique_ptr<tcpframework::SendSocket> send;
+		std::unique_ptr<tcpframework::ClientSocket> client;
 	public:
 
 		void Init() {
 			siv::Println(tcpframework::TcpManager::Init());
-			server = std::make_unique<tcpframework::ServerSocket>(19132,5);
-			siv::Println(server->Bind());
-			siv::Println(server->Listen());
-			send=server->Accept();
-			send->Send("Ç†ÇÌÇ†Ç†Ç†Ç†Ç†ÅI");
-			
+			client = std::make_unique<tcpframework::ClientSocket>(80, "yahoo.co.jp");
+			siv::Println(client->Connect());
+			client->Send(
+				"GET  HTTP/1.0\r\n\r\n"
+			);
+			std::ofstream ofs("html.html");
+			while (client->Receive() > 0) {
+				auto bytes = client->GetBuf();
+				std::string s(bytes.GetBytes(), bytes.Size());
+			//	siv::Println(siv::CharacterSet::Widen(s));
+				ofs << s;
+			}
+			ofs.close();
 		}
 		void Update() {
-			if (send->Receive()==-1)
+		/*	if (client->Receive()==-1)
 				return;
-			auto bytes = send->GetBuf();
+			auto bytes = client->GetBuf();
 			std::string s(bytes.GetBytes(),bytes.Size());
 			siv::Println(siv::CharacterSet::Widen(s));
+			siv::Println(L"adfaf");*/
 		}
 		void End() {
-			send->Close();
-			server->Close();
+			client->Close();
 			tcpframework::TcpManager::End(); 
 		}
 	};
